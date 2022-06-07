dc24f24ff8c4f585b09cc61c06dddc3d45f6d956
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-06-08 01:02:05 +0500

    Write Concise Object Literal Declarations Using Object Property Shorthand

---
+++ ES6/Write Concise Object Literal Declarations Using Object Property Shorthand.js
@@ -0,0 +1,10 @@
+const createPerson = (name, age, gender) => ({
+    // Only change code below this line
+      name,
+      age,
+      gender
+    
+    // Only change code above this line
+  });
+  let data=createPerson("Zodiac Hasbro", 56, "male")
+  console.log(data)
