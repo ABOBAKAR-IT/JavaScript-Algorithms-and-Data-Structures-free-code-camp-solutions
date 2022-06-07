cf9cfcb81dad47d379035ca8f4d4782705cf6bbd
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-06-08 00:22:28 +0500

    Create Strings using Template Literals

---
+++ ES6/Create Strings using Template Literals.js
@@ -0,0 +1,18 @@
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    for(let i=0;i<arr.length;i++)
+    {
+      failureItems[i]=`<li class=\"text-warning\">${arr[i]}</li>`
+    }
+    // Only change code above this line
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
